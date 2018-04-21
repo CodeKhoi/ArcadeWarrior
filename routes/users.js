f2573c6d3b845a62629b41efb01a05f594511dee
CodeKhoi <30888184+CodeKhoi@users.noreply.github.com>
2018-04-20 22:15:36 -0700

    test

--- routes/users.js
+++ routes/users.js
@@ -102,7 +102,7 @@ passport.deserializeUser(function(id, done) {
 });
 
 router.post('/login',
-  passport.authenticate('local', {successRedirect:'/users/mario', failureRedirect:'/users/login',failureFlash: true}),
+  passport.authenticate({successRedirect:'/users/mario', failureRedirect:'/users/login',failureFlash: true}),
   function(req, res) {
   res.redirect('/');
  });
